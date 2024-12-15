5c4d8db2751751463c215573b360a55a9032a27e
GitSRealpe <msn-realpe@hotmail.com>
2024-12-15 18:06:32 +0100

    peixos move in realtime now

--- scripts/stonefish_boids.cpp
+++ scripts/stonefish_boids.cpp
@@ -9,7 +9,7 @@
 #include <Stonefish/entities/solids/Sphere.h>
 #include <Stonefish/entities/solids/Box.h>
 #include <Stonefish/entities/AnimatedEntity.h>
-#include <Stonefish/entities/animation/BSTrajectory.h>
+#include <Stonefish/entities/animation/ManualTrajectory.h>
 
 #include <cirs_boids/Flock.hpp>
 
@@ -19,7 +19,18 @@ private:
     ros::ServiceClient client;
     ros::ServiceServer service;
 
+    const int numBoids = 20;
+    const double worldSize = 2.0;
+    const double dt = 0.1;
+    std::unique_ptr<Flock> flock;
+
     sf::AnimatedEntity *anim;
+    sf::ManualTrajectory *leader_traj;
+    std::vector<sf::ManualTrajectory *> boid_paths;
+
+    ros::Timer timer;
+    void timerCb(const ros::TimerEvent &evt);
+    float inc = 0;
 
 public:
     MyManager(sf::Scalar rate, std::string scenarioPath);
@@ -34,19 +45,54 @@ MyManager::MyManager(sf::Scalar rate, std::string scenarioPath) : ROSSimulationM
     std::cout << "inherited rosmanager" << "\n";
     std::cout << rate << "\n";
     std::cout << scenarioPath << "\n";
+
+    leader_traj = new sf::ManualTrajectory();
+    timer = nh.createTimer(ros::Duration(1.0 / 30.0), &MyManager::timerCb, this);
 }
 
 MyManager::~MyManager()
 {
 }
 
+void MyManager::timerCb(const ros::TimerEvent &evt)
+{
+
+    // flock->setLeaderPosition(Vector3f(5 * sin(inc), 5 * cos(inc), sin(inc) + 2));
+    flock->setLeaderPosition(Vector3f(0, 0, 2));
+    flock->updateBoids(1.0 / 30.0);
+    auto leader_pos = flock->getLeaderPosition();
+    sf::Vector3 point(leader_pos.x(), leader_pos.y(), leader_pos.z());
+    sf::Quaternion ori(0, 0, 0, 1);
+    leader_traj->setTransform(sf::Transform(ori, point));
+
+    for (int i = 0; i < flock->getBoids().size(); i++)
+    {
+        auto boid = flock->getBoids().at(i);
+        sf::Vector3 point(boid.position.x(), boid.position.y(), boid.position.z());
+
+        // Orientation as quaternion with no roll
+        Vector3f forward = boid.velocity.normalized();   // Direction of movement
+        Vector3f up = Vector3f::UnitZ();                 // Fixed "up" direction
+        Vector3f right = up.cross(forward).normalized(); // Perpendicular right vector
+        up = forward.cross(right).normalized();          // Recompute orthogonal up vector
+        Eigen::Matrix3f rotationMatrix;
+        rotationMatrix.col(0) = forward; // Forward direction
+        rotationMatrix.col(1) = right;   // Right direction
+        rotationMatrix.col(2) = up;      // Up direction
+        Eigen::Quaternionf orientation(rotationMatrix);
+        sf::Quaternion ori(orientation.x(), orientation.y(),
+                           orientation.z(), orientation.w());
+
+        boid_paths.at(i)->setTransform(sf::Transform(ori, point));
+    }
+
+    inc++;
+}
+
 void MyManager::MakeFishes()
 {
 
-    const int numBoids = 20;
-    const double worldSize = 2.0;
-    const double dt = 0.1;
-    Flock flock(numBoids, worldSize, Vector3f(0, 0, 0));
+    flock = std::make_unique<Flock>(numBoids, worldSize, Vector3f(0, 0, 0));
 
     XmlRpc::XmlRpcValue boids_cfg;
     bool gotten = nh.getParam("/boids_settings", boids_cfg);
@@ -57,77 +103,26 @@ void MyManager::MakeFishes()
         std::cout << "found: " << boids_cfg[b]["name"] << "\n";
         std::cout << boids_cfg[b]["name"] << "\n";
         std::cout << static_cast<double>(boids_cfg[b]["alignmentWeight"]) << "\n";
-        flock.setAlignmentWeight(static_cast<double>(boids_cfg[b]["alignmentWeight"]));
-        flock.setCohesionWeight(static_cast<double>(boids_cfg[b]["cohesionWeight"]));
-        flock.setSeparationWeight(static_cast<double>(boids_cfg[b]["separationWeight"]));
-        flock.setLeaderAttractionWeight(static_cast<double>(boids_cfg[b]["leaderAttractionWeight"]));
-        flock.setMaxSpeed(static_cast<double>(boids_cfg[b]["maxSpeed"]));
-        flock.setPerceptionRadius(static_cast<double>(boids_cfg[b]["perceptionRadius"]));
-        flock.setSeparationRadius(static_cast<double>(boids_cfg[b]["separationRadius"]));
+        flock->setAlignmentWeight(static_cast<double>(boids_cfg[b]["alignmentWeight"]));
+        flock->setCohesionWeight(static_cast<double>(boids_cfg[b]["cohesionWeight"]));
+        flock->setSeparationWeight(static_cast<double>(boids_cfg[b]["separationWeight"]));
+        flock->setLeaderAttractionWeight(static_cast<double>(boids_cfg[b]["leaderAttractionWeight"]));
+        flock->setMaxSpeed(static_cast<double>(boids_cfg[b]["maxSpeed"]));
+        flock->setPerceptionRadius(static_cast<double>(boids_cfg[b]["perceptionRadius"]));
+        flock->setSeparationRadius(static_cast<double>(boids_cfg[b]["separationRadius"]));
 
-        flock.printSettings();
+        flock->printSettings();
         break;
     }
 
-    std::vector<sf::BSTrajectory *> boid_paths;
-    for (int i = 0; i < numBoids; i++)
-    {
-        sf::BSTrajectory *traj = new sf::BSTrajectory(sf::PlaybackMode::REPEAT);
-        boid_paths.push_back(traj);
-    }
-
-    sf::BSTrajectory *leader_traj = new sf::BSTrajectory(sf::PlaybackMode::REPEAT);
-
-    double t = dt;
-    int iterations = 0;
-    while (iterations < 200)
-    {
-        flock.setLeaderPosition(Vector3f(5 * sin(t / 5), 5 * cos(t / 5), sin(t) + 2));
-        // flock.setLeaderPosition(Vector3f(0, 0, 2));
-        auto leader_pos = flock.getLeaderPosition();
-        leader_traj->AddKeyPoint(t, sf::Transform(sf::Quaternion(0, 0, 0, 1),
-                                                  sf::Vector3(leader_pos.x(),
-                                                              leader_pos.y(),
-                                                              leader_pos.z())));
-        flock.updateBoids(dt);
-
-        for (int i = 0; i < flock.getBoids().size(); i++)
-        {
-            auto boid = flock.getBoids().at(i);
-            sf::Vector3 point(boid.position.x(), boid.position.y(), boid.position.z());
-
-            // // Orientation as quaternion
-            // Vector3f direction = boid.velocity.normalized();
-            // Eigen::Quaternionf orientation;
-            // orientation.setFromTwoVectors(Vector3f::UnitX(), direction);
-
-            // Orientation as quaternion with no roll
-            Vector3f forward = boid.velocity.normalized();   // Direction of movement
-            Vector3f up = Vector3f::UnitZ();                 // Fixed "up" direction
-            Vector3f right = up.cross(forward).normalized(); // Perpendicular right vector
-            up = forward.cross(right).normalized();          // Recompute orthogonal up vector
-            Eigen::Matrix3f rotationMatrix;
-            rotationMatrix.col(0) = forward; // Forward direction
-            rotationMatrix.col(1) = right;   // Right direction
-            rotationMatrix.col(2) = up;      // Up direction
-            Eigen::Quaternionf orientation(rotationMatrix);
-            sf::Quaternion ori(orientation.x(), orientation.y(),
-                               orientation.z(), orientation.w());
-
-            boid_paths.at(i)->AddKeyPoint(t, sf::Transform(ori, point));
-        }
-
-        t += 0.1;
-        iterations++;
-    }
-
-    std::cout << boid_paths.size() << "\n";
-
     CreateLook("fishtex", sf::Color::Gray(1.f), 0.1f, 0.f, 0.f,
                sf::GetDataPath() + "creatures/yellowtang/yellowtang.png");
-
+    // create control ptr for every boid
     for (int i = 0; i < numBoids; i++)
     {
+        sf::ManualTrajectory *traj = new sf::ManualTrajectory();
+        boid_paths.push_back(traj);
+
         anim = new sf::AnimatedEntity("AnimMesh" + std::to_string(i), boid_paths.at(i),
                                       sf::GetDataPath() + "creatures/yellowtang/yellowtang.obj",
                                       1.0, sf::I4(),
@@ -136,13 +131,9 @@ void MyManager::MakeFishes()
         AddAnimatedEntity(anim);
     }
 
+    std::cout << boid_paths.size() << "\n";
     anim = new sf::AnimatedEntity("LeaderFrame", leader_traj);
     AddAnimatedEntity(anim);
-
-    // sf::AnimatedEntity *anim = new sf::AnimatedEntity("Frame", traj);
-
-    // std::cout << getEntity("AnimMesh")->getName() << "\n";
-    //  dynamic_cast<sf::AnimatedEntity *>(getEntity("AnimMesh"));
 }
 
 void MyManager::BuildScenario()
